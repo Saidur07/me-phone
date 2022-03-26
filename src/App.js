f3e29d3857e2198ec663afa51573a59345874a7c
Saidur-07 <md.saidurrahman.bhola2007@gmail.com>
2022-03-26 19:57:42 +0600

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="footer mt-auto py-3 bg-light">
+      <div class="container">
+        <span class="text-muted text-black">
+          © SR7 Ultimate 2022, All Rights Reserved
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
